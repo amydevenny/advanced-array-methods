3de07c36c08cb6d4830b0613a3a4391cbbbf7f3e
Amy <devennyamy@gmail.com>
2023-03-11 12:52:25 -0500

    find and find index

--- find-findindex/app.js
+++ find-findindex/app.js
@@ -46,6 +46,12 @@ const evenScores =  scores.filter(function(score) {
   return score !== 0 && score % 2 === 0;
  })
 
+ // find a score that doesn't exist
+ // returns -1 because it's looking for an index
+ scores.findIndex(function(score){
+  return score > 100;
+ })
+
  // split an array at a pivot point
  // takes in 2 parameters, the array and the pivot value
  function partition(arr, pivot) {
@@ -60,3 +66,35 @@ const evenScores =  scores.filter(function(score) {
   //return a new array containing both left and right arrays
   return [ left, right ];
  }
+
+ //rewrite my own versions of find
+ // accepts an array and a callback as parameters
+function myFind(arr, callback) {
+  //loops through the array
+  for (let i = 0; i < arr.length; i++) {
+    // pass in the value, index, and entire array to the callback
+    // if the callback is true at any point, return the value of that index
+    // if nothing is true it returns undefined by default
+    if (callback(arr[i], i, arr) === true) return arr[i];
+  }
+}
+
+// run myFind on scores array, looking for a score greater than 91
+myFind(scores, function(score){
+  return score > 91;
+})
+
+//rewrite my own versions of findIndex
+// same as findIndex
+function myFindIndex(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    //except return the first index that is true
+    if (callback(arr[i], i, arr) === true) return i;
+  }
+  //return -1 if nothing is true
+  return -1;
+}
+
+myFindIndex(scores, function(score){
+  return score !== 0 && score % 2 === 0;
+})
